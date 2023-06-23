1afc9bc98fd0501a547218995b4ec8484ab08306
Elvyn Mejia <elvynmejia@gmail.com>
2023-06-23 14:15:41 -0700

    initial commit

---
+++ src/components/Chat.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = {
+    source: 'ai',
+    message: 'Welcome to the chap app. How can I help you with'
+};
+
+const messages = ref([message]);
+</script>
+
+<template>
+    <h1>Welcome</h1>
+    <div>
+        {{ JSON.stringify(messages) }}
+    </div>
+</template>
